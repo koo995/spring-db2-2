4ffd9d74d635d03915c16c5ea47e0f997b22ebc2
koo995 <107671886+koo995@users.noreply.github.com>
2024-04-11 15:25:58 +0900

    트랜잭션 적용 확인

---
+++ src/test/java/com/example/springtx/apply/TxBasicTest.java
@@ -0,0 +1,64 @@
+package com.example.springtx.apply;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+@SpringBootTest
+public class TxBasicTest {
+
+    @Autowired BasicService basicService;
+
+    /**
+     * AopUtils.isAopProxy 를 통해 프록시인지 확인할 수 있다.
+     * @Transactional 이 붙어있지 않으면 프록시가 아니다.
+     */
+    @Test
+    void proxyCheck() {
+        log.info("aop class={}", basicService.getClass());
+        assertThat(AopUtils.isAopProxy(basicService)).isTrue();
+    }
+
+    @Test
+    void txTest() {
+        basicService.tx();
+        basicService.nonTx();
+    }
+
+    @TestConfiguration
+    static class TxApplyBasicConfig {
+        @Bean
+        BasicService basicService() {
+            return new BasicService();
+        }
+    }
+
+    @Slf4j
+    static class BasicService {
+
+        /**
+         * isActualTransactionActive 이것을 확인하면 tx가 활성화 되었는지 아닌지 확인할 수 있다.
+         */
+        @Transactional
+        public void tx() {
+            log.info("call tx");
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active={}", txActive);
+        }
+
+        public void nonTx() {
+            log.info("call nonTx");
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active={}", txActive);
+        }
+    }
+}
