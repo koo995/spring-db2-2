2cb0f759d3a20cc1580a1c09c27da3789a09662c
koo995 <107671886+koo995@users.noreply.github.com>
2024-04-11 17:59:00 +0900

    트랜잭션 aop 주의 사항 - 프록시 내부 호출2

---
+++ src/test/java/com/example/springtx/apply/InternalCallV2Test.java
@@ -0,0 +1,83 @@
+package com.example.springtx.apply;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@Slf4j
+@SpringBootTest
+public class InternalCallV2Test {
+
+    @Autowired
+    CallService callService;
+
+    @Test
+    void printProxy() {
+        log.info("callService class={}", callService.getClass());
+    }
+
+    @Test
+    void externalCallV2() {
+        callService.external();
+    }
+
+    @TestConfiguration
+    static class InternalCallV1TestConfig {
+
+        @Bean
+        CallService callService() {
+            return new CallService(internalService());
+        }
+
+        @Bean
+        InternalService internalService() {
+            return new InternalService();
+        }
+
+    }
+
+    @Slf4j
+    @RequiredArgsConstructor
+    static class CallService {
+
+        /**
+         * 분리한 내부호출 클래스를 주입받는다.
+         * final 이 있으니까 생성자 주입을 컴파일 시점에 깜빡하지 않고 체크할 수 있다.
+         */
+        private final InternalService internalService;
+
+        public void external() {
+            log.info("call external");
+            printTxInfo();
+            internalService.internal();
+        }
+
+        private void printTxInfo() {
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active={}", txActive);
+        }
+    }
+
+    /**
+     * 가장 간단한 방법은 내부호출을 분리하는 것이다.
+     */
+    static class InternalService {
+
+        @Transactional
+        public void internal() {
+            log.info("call internal");
+            printTxInfo();
+        }
+
+        private void printTxInfo() {
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active={}", txActive);
+        }
+    }
+}
