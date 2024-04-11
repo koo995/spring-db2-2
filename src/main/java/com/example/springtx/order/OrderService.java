4866e72fd6619209fa281172d0a3687617b0ec12
koo995 <107671886+koo995@users.noreply.github.com>
2024-04-11 21:16:06 +0900

    jpa의 쿼리가 안날라가는 이유

--- src/main/java/com/example/springtx/order/OrderService.java
+++ src/main/java/com/example/springtx/order/OrderService.java
@@ -12,7 +12,12 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
 
-    //JPA는 트랜잭션 커밋 시점에 Order 데이터를 DB에 반영한다.
+    /**
+     * JPA는 트랜잭션 커밋 시점에 Order 데이터를 DB에 반영한다.
+     * 테스트 도중 하나 궁금한 것이 발생할 수 있다.
+     * 롤백이 발생했을때 save() 메서드에 의해서 insert 쿼리가 발생해야 하는것이 아닌지 의문이 생길 수 있다.
+     * jpa 는 커밋될때 쿼리가 날라가는 것이 있다. 어짜피 롤백이 되면 쿼리 자체가 날라가지 않는다.
+     */
     @Transactional
     public void order(Order order) throws NotEnoughMoneyException {
         log.info("order 호출");
