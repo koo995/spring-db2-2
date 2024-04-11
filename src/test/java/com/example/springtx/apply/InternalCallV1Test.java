44ace45c5ecf8eb806035d29002d75947389329a
koo995 <107671886+koo995@users.noreply.github.com>
2024-04-11 16:32:42 +0900

    트랜잭션 aop 주의 사항 - 프록시 내부 호출1

---
+++ src/test/java/com/example/springtx/apply/InternalCallV1Test.java
@@ -0,0 +1,69 @@
+package com.example.springtx.apply;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@Slf4j
+@SpringBootTest
+public class InternalCallV1Test {
+
+    @Autowired
+    CallService callService;
+
+    @Test
+    void printProxy() {
+        log.info("callService class={}", callService.getClass());
+    }
+
+    @Test
+    void internalCall() {
+        callService.internal();
+    }
+
+    /**
+     * 외부에서 호출하면 트랜잭션이 적용되지 않는다.
+     */
+    @Test
+    void externalCall() {
+        callService.external();
+    }
+
+    @TestConfiguration
+    static class InternalCallV1TestConfig {
+
+        @Bean
+        CallService callService() {
+            return new CallService();
+        }
+    }
+
+    @Slf4j
+    static class CallService {
+
+        public void external() {
+            log.info("call external");
+            printTxInfo();
+            /**
+             * this 는 나 자신의 인스턴스 주소이다.
+             */
+            this.internal();
+        }
+
+        @Transactional
+        public void internal() {
+            log.info("call internal");
+            printTxInfo();
+        }
+
+        private void printTxInfo() {
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active={}", txActive);
+        }
+    }
+}
