1c4fea68d4b48768e1dedfd8308894794a3d19c6
koo995 <107671886+koo995@users.noreply.github.com>
2024-04-12 18:04:31 +0900

    트랜잭션 전파 활용1 - 예제 프로젝트 시작

---
+++ src/main/java/com/example/springtx/propagation/MemberService.java
@@ -0,0 +1,55 @@
+package com.example.springtx.propagation;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+    private final LogRepository logRepository;
+
+    /**
+     * 각 리포지토리가 서로 다른 트랜잭션을 사용한다.
+     */
+    public void joinV1(String username) {
+        Member member = new Member(username);
+        Log logMessage = new Log(username);
+
+        log.info("== memberRepository 호출 시작 ==");
+        memberRepository.save(member);
+        log.info("== memberRepository 호출 종료 ==");
+
+        log.info("== logRepository 호출 시작 ==");
+        logRepository.save(logMessage);
+        log.info("== logRepository 호출 종료 ==");
+    }
+
+    /**
+     * 로그에 예외가 터지면 메서드 밖으로 던지는 것 보다 그냥 잡아서 처리하자. 안그러면 예외의 화면이 고객에게 날라간다.
+     * 로그저장에 실패했지만 예외 잡아채고 정상흐름으로 전환. (그냥 로그를 따로 기록하고 저장한다는 정책을 쓴다고 가정)
+     */
+    public void joinV2(String username) {
+        Member member = new Member(username);
+        Log logMessage = new Log(username);
+
+        log.info("== memberRepository 호출 시작 ==");
+        memberRepository.save(member);
+        log.info("== memberRepository 호출 종료 ==");
+
+        log.info("== logRepository 호출 시작 ==");
+        try {
+            logRepository.save(logMessage);
+        } catch (RuntimeException e) {
+            log.info("log 저장에 실패했습니다. logMessage={}", logMessage.getMessage());
+            log.info("정상 흐름 반환");
+        }
+
+        log.info("== logRepository 호출 종료 ==");
+    }
+
+}
