4b9c00650433baa311592e208bee28dca6c47d5e
koo995 <107671886+koo995@users.noreply.github.com>
2024-04-12 20:39:47 +0900

    트랜잭션 전파 활용6 - 복구 REQUIRED

--- src/main/java/com/example/springtx/propagation/MemberService.java
+++ src/main/java/com/example/springtx/propagation/MemberService.java
@@ -34,6 +34,7 @@ public class MemberService {
      * 로그에 예외가 터지면 메서드 밖으로 던지는 것 보다 그냥 잡아서 처리하자. 안그러면 예외의 화면이 고객에게 날라간다.
      * 로그저장에 실패했지만 예외 잡아채고 정상흐름으로 전환. (그냥 로그를 따로 기록하고 저장한다는 정책을 쓴다고 가정)
      */
+    @Transactional
     public void joinV2(String username) {
         Member member = new Member(username);
         Log logMessage = new Log(username);
